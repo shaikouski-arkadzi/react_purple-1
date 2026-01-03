0623c90e85bbbd74b9e6d996cecac4f5b95f3ad5
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-01-03 12:57:12 +0300

    Add SelectUser component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,11 @@
+import SelectUser from "../SelectUser/SelectUser";
 import styles from "./Header.module.css";
 
 export function Header() {
-  return <img className={styles.logo} src="/logo.svg" alt="Логотип журнала" />;
+  return (
+    <>
+      <img className={styles.logo} src="/logo.svg" alt="Логотип журнала" />
+      <SelectUser />
+    </>
+  );
 }
