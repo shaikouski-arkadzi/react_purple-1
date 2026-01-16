22bf115c4a62e64f446474a667bd0bbdf14ed21b
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-01-16 20:49:57 +0300

    Refactor: rename text to post variable

--- src/components/JournalItem/JournalItem.jsx
+++ src/components/JournalItem/JournalItem.jsx
@@ -1,6 +1,6 @@
 import "./JournalItem.css";
 
-export function JournalItem({ title, text, date }) {
+export function JournalItem({ title, post, date }) {
   const formatedDate = new Intl.DateTimeFormat("ru-RU").format(date);
 
   return (
@@ -8,7 +8,7 @@ export function JournalItem({ title, text, date }) {
       <h2 className="journal-item__header">{title}</h2>
       <h2 className="journal-item__body">
         <div className="journal-item__date">{formatedDate}</div>
-        <div className="journal-item__text">{text}</div>
+        <div className="journal-item__text">{post}</div>
       </h2>
     </>
   );
